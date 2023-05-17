e448287dc95b18f846ffc53a20a4fc157cedb92d
tranduyhung12bbm <tranduyhung12bbm@gmail.com>
2023-05-17 21:15:09 +0700

    first commit

--- src/components/TodoApp/TodoApp.js
+++ src/components/TodoApp/TodoApp.js
@@ -1,31 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // import './App.css';
-import './TodoApp.css'
-const TodoApp = () =>  {
-  const [tasks, setTasks] = useState([]);
-  const [currentTask, setCurrentTask] = useState('');
-  const [filter, setFilter] = useState('all');
+import "./TodoApp.css";
+const TodoApp = () => {
+  const [tasks, setTasks] = useState(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    return storedTasks ? JSON.parse(storedTasks) : [];
+  });
+  const [currentTask, setCurrentTask] = useState("");
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
     if (storedTasks) {
       setTasks(storedTasks);
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
   const addTask = () => {
-    if (currentTask.trim() !== '') {
+    if (currentTask.trim() !== "") {
       const newTask = {
         id: Date.now(),
         title: currentTask,
         completed: false,
       };
       setTasks([...tasks, newTask]);
-      setCurrentTask('');
+      setCurrentTask("");
     }
   };
 
@@ -49,9 +52,9 @@ const TodoApp = () =>  {
 
   const filterTasks = () => {
     switch (filter) {
-      case 'active':
+      case "active":
         return tasks.filter((task) => !task.completed);
-      case 'completed':
+      case "completed":
         return tasks.filter((task) => task.completed);
       default:
         return tasks;
@@ -60,50 +63,46 @@ const TodoApp = () =>  {
 
   return (
     <div className="App">
-      <h1>Todo App</h1>
+      <h1>#todo</h1>
       <div className="filter-container">
         <button
-          className={filter === 'all' ? 'active' : ''}
-          onClick={() => setFilter('all')}
+          className={filter === "all" ? "active" : ""}
+          onClick={() => setFilter("all")}
         >
           All
         </button>
         <button
-          className={filter === 'active' ? 'active' : ''}
-          onClick={() => setFilter('active')}
+          className={filter === "active" ? "active" : ""}
+          onClick={() => setFilter("active")}
         >
           Active
         </button>
         <button
-          className={filter === 'completed' ? 'active' : ''}
-          onClick={() => setFilter('completed')}
+          className={filter === "completed" ? "active" : ""}
+          onClick={() => setFilter("completed")}
         >
           Completed
         </button>
-        <button className="clear-button" onClick={() => setTasks([])}>
-        Clear All
-      </button>
       </div>
       <div className="input-container">
         <input
+          className="form-control me-2"
           type="text"
           placeholder="Add a task"
           value={currentTask}
           onChange={(e) => setCurrentTask(e.target.value)}
         />
-        <button onClick={addTask}>Add Task</button>
-        
-      
+        <button className="btn btn-outline-success" onClick={addTask}>
+          Add
+        </button>
       </div>
-     
-
 
       <div className="tasks-container">
         <ul>
           {filterTasks().map((task) => (
             <li key={task.id}>
               <span
-                className={task.completed ? 'completed' : ''}
+                className={task.completed ? "completed" : ""}
                 onClick={() => completeTask(task.id)}
               >
                 {task.title}
@@ -114,9 +113,14 @@ const TodoApp = () =>  {
         </ul>
       </div>
 
-     
+      <div
+        className="d-grid gap-2 d-md-flex justify-content-md-end"
+        onClick={() => setTasks([])}
+      >
+        <button className="btn btn-danger me-md-2">Delete All</button>
+      </div>
     </div>
   );
-}
+};
 
 export default TodoApp;
